8d8e1e37d7ff26582ed156923db617a81ddf956c
Kushdeep Mittal <Kushdeep.Mittal@docusign.com>
2025-09-18 00:19:14 +0530

    chnge

--- server.js
+++ server.js
@@ -9,6 +9,14 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+// Allow website to be embedded in iframe
+app.use((req, res, next) => {
+  res.setHeader('X-Frame-Options', 'ALLOWALL');
+  res.setHeader('Content-Security-Policy', "frame-ancestors *");
+  next();
+});
+
 app.use(express.static('public'));
 
 // Initialize SQLite database
